b15aefd4de02ab7e47bbd249f898effa5dac1bfd
AoralsFout <102973374+AoralsFout@users.noreply.github.com>
2022-10-02 09:06:51 +0800

    Add files via upload

--- tools/Response speed/index.js
+++ tools/Response speed/index.js
@@ -4,7 +4,7 @@ var startPressed = false;
 var bgChangeStarted = false;
 var maxWait = 20;
 var timerID;
-var colors = new Array("chartreuse");
+var colors = new Array("rgb(75, 219, 106)");
 function tips_error(t) {
     $("#validate-loading").hide(),
     $("#validate-result").empty(),
@@ -26,10 +26,11 @@ function stopTest() {
     if (bgChangeStarted) {
         $("#btn-start").attr("disabled", false);
         endTime = new Date();
-        var responseTime = (endTime.getTime() - startTime.getTime()) / 1000;
-        $('#color-div').css({ "background-color": "white" });
+        var responseTime = (endTime.getTime() - startTime.getTime());
+        document.getElementById('time').innerHTML = responseTime + ' ms !';
+        $('#color-div').css({ "background-color": "#ffffff00" });
         $('#tool-result').show();
-        $('#tool-result').append("你的反应时间是: " + responseTime + " s" + "<br/>");
+        $('#tool-result').append("你的反应时间是: " + responseTime + " ms" + "<br/>");
         startPressed = false;
         bgChangeStarted = false;
         $("#btn-stop").attr("disabled", true);
@@ -40,10 +41,15 @@ function stopTest() {
         } else {
             clearTimeout(timerID);
             startPressed = false;
+            document.getElementById('time').innerHTML = "太早了！别想预判我！" ;
             tips_error("可别作弊哦，颜色还没有开始变呢,请重新开始！");
             $("#btn-start").attr("disabled", false);
         }
     }
+    document.getElementById('startTest').style.setProperty('display','block');
+    document.getElementById('time').style.setProperty('display','block');
+    document.getElementById('maintips').style.setProperty('display','none');
+    document.getElementById('mainbackground').style.setProperty('background-color','rgb(43, 135, 209)');
 }
 
 var randMULTIPLIER = 0x015a4e35;
@@ -67,4 +73,9 @@ function start() {
         startPressed = true;
         timerID = setTimeout('startTest()', 6000 * randNumber());
     }
+    document.getElementById('mainbackground').style.setProperty('background-color','rgb(206, 38, 54)');
+    document.getElementById('maintips').style.setProperty('display','block');
+    document.getElementById('startTest').style.setProperty('display','none');
+    document.getElementById('time').style.setProperty('display','none');
+    document.getElementById('maintips').innerHTML = '请等待背景颜色变为绿色' ;
 }
